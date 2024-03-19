a84748b3fd01c354932e31bc91576ee98f00fe75
maxwellwachira <maxwellwachira67@gmail.com>
2024-03-19 23:48:30 +0300

    changed month from long to short

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -8,7 +8,7 @@ const formatNumberWithCommas = (number: number | undefined): string => {
 const displayMonthAndYear = (dateString: Date | undefined): string =>{
     if(dateString){
         const date = new Date(dateString);
-       return  `${date.toLocaleString('en', { month: 'long' })} ${date.getFullYear()}`;
+       return  `${date.toLocaleString('en', { month: 'short' })} ${date.getFullYear()}`;
     }
 
     return "";
