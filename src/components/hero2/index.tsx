8c314c185a7ea30b375b2f99dbd6d8d904684553
maxwellwachira <maxwellwachira67@gmail.com>
2024-03-26 20:00:03 +0300

    updated from background color to background image

--- src/components/hero2/index.tsx
+++ src/components/hero2/index.tsx
@@ -1,47 +1,49 @@
-import { Box, Group, Grid, Stack, Text } from "@mantine/core";
+import { Box, Group, Grid, Stack, Text, BackgroundImage, Container } from "@mantine/core";
 import classes from './Hero.module.css'
 import { useMediaQuery } from "@mantine/hooks";
 import Image from "next/image";
 import apartmentImage from "@/assets/apt.png"
 import { SearchBar } from "../searchbar";
-
+import sky from "@/assets/sky.jpg";
 
 const HomePageHero = () => {
     const smBreakPoint = useMediaQuery('(max-width: 48em)');
     const mdBreakPoint = useMediaQuery('(max-width: 62em)');
     return (
-        <Box className={classes.heroBackground} mt={8}>
-            <Grid maw={1295} mah={600}>
-                <Grid.Col span={{ base: 12, md: 6 }}>
-                    <Stack justify="center" style={{ height: "70%" }} mt={smBreakPoint ? 40 : mdBreakPoint ? 60: 0}>
-                        <Text className={classes.title}>Unlock Your Perfect Space: Buy or Rent, Your Property Awaits!</Text>
-                        <Text className={classes.subtitle}>Find the perfect space to call your own or rent. Start your journey to finding your dream property today!</Text>
-                    </Stack>
-                </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 6 }}>
-                    {!(smBreakPoint || mdBreakPoint) &&
-                        <Stack align="center" >
-                            <Image
-                                src={apartmentImage}
-                                alt="Apartment Image"
-                                width={650}
-                                height={600}
-                            />
+        <Container size={1350}>
+            <BackgroundImage className={classes.heroBackground} mt={8} src={sky.src}>
+                <Grid maw={1295} mah={600}>
+                    <Grid.Col span={{ base: 12, md: 6 }}>
+                        <Stack justify="center" style={{ height: "70%" }} mt={smBreakPoint ? 40 : mdBreakPoint ? 60 : 0}>
+                            <Text className={classes.title}>Unlock Your Perfect Space: Buy or Rent, Your Property Awaits!</Text>
+                            <Text className={classes.subtitle}>Find the perfect space to call your own or rent. Start your journey to finding your dream property today!</Text>
                         </Stack>
-                    }
-                </Grid.Col>
-            </Grid>
-            {(smBreakPoint || mdBreakPoint) ?
-                <Box mt={mdBreakPoint ? 50 : 0}>
-                    <SearchBar />
-                </Box> :
-                <Stack align="center" style={{ position: "relative" }}>
-                    <Box className={classes.searchPosition}>
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 6 }}>
+                        {!(smBreakPoint || mdBreakPoint) &&
+                            <Stack align="center" >
+                                <Image
+                                    src={apartmentImage}
+                                    alt="Apartment Image"
+                                    width={650}
+                                    height={600}
+                                />
+                            </Stack>
+                        }
+                    </Grid.Col>
+                </Grid>
+                {(smBreakPoint || mdBreakPoint) ?
+                    <Box mt={mdBreakPoint ? 50 : 0}>
                         <SearchBar />
-                    </Box>
-                </Stack>
-            }
-        </Box>
+                    </Box> :
+                    <Stack align="center" style={{ position: "relative" }}>
+                        <Box className={classes.searchPosition}>
+                            <SearchBar />
+                        </Box>
+                    </Stack>
+                }
+            </BackgroundImage>
+        </Container>
     )
 }
 
